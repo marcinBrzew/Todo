93db00fd5b96d43ece516515bb6493c6c72946a5
Marcin <marcinbrzewinski@o2.pl>
2016-09-22 21:47:03 +0200

    initial commit

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,14 @@
+@extends('layout')
+
+@section('content')
+    <h1>The About page</h1>
+
+@stop
+
+@section('footer')
+        <script type="text/javascript">
+            alert('About page')
+        </script>
+
+
+@stop
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+@extends('layout')
+
+@section('header')
+
+<link rel="stylesheet" type="text/css"
+          href="https://fonts.googleapis.com/css?family=Tangerine">
+
+
+@section('content')
+    <h1>The Welcome page</h1>
+
+@stop
